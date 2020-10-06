2891e9d93f2bedede43829cbddd1604651b10943
Nikhil Dingane <nikhil@krishagni.com>
2020-10-06 19:31:13 +0530

    Solved packaging cupcakes

---
+++ packagingcupcakes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	unsigned long long N;
+
+    cin>>T;
+
+    while(T-- > 0){
+    	cin>>N;
+    	cout<<1+(N/2)<<endl;
+    }
+	return 0;
+}
