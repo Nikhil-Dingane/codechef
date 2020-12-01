79a50726f5e303d04be2dedc0c599b103562322f
Nikhil Dingane <nikhil@krishagni.com>
2020-12-01 18:26:39 +0530

    Solved life, the universe and everything

---
+++ lifetheuniverseandeverything.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/TEST
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int testCases = 0, no = 0;
+    while(true){
+        cin>>no;
+        if(no == 42){
+            break;
+        }
+        cout<<no<<endl;
+        no = 0;
+    }
+    return 0;
+}
+
